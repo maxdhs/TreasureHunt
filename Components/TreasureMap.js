3a751d9fef5052d5feb66f23953f1b92c81362c1
Maxwell Dubin <maxdhs@gmail.com>
2019-07-28 16:09:39 -0700

    Add custom marker icons, alerts if treasure found, zoom to treasure chest markers

--- Components/TreasureMap.js
+++ Components/TreasureMap.js
@@ -1,38 +1,69 @@
 import React from "react";
 import MapView, { Marker } from "react-native-maps";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Image } from "react-native";
 import { connect } from "react-redux";
-import { fetchSites } from "../Redux/sites";
+import { fetchSites, fetchTreasure } from "../Redux/sites";
 
 class TreasureMap extends React.Component {
   constructor(props) {
     super(props);
+    this.mapRef = null;
   }
 
   componentDidMount = () => {
     this.props.fetchSites();
   };
 
+  componentDidUpdate = () => {
+    this.zoomInOnMarkers();
+  };
+
+  handlePress = id => {
+    this.props.siteHasTreasure(id);
+  };
+
   generateMarkers = () => {
     const markers = this.props.sites.map(site => (
       <Marker
+        onPress={() => this.handlePress(site.id)}
         key={site.id}
         coordinate={{
           latitude: site.latitude,
           longitude: site.longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
+          latitudeDelta: 0.02,
+          longitudeDelta: 0.02
         }}
-        title="TITLE"
-        description="DESCRIPTION"
-      />
+      >
+        <Image
+          source={require("../assets/chest.png")}
+          style={{ height: 35, width: 35 }}
+        />
+      </Marker>
     ));
     return markers;
   };
 
+  zoomInOnMarkers = () => {
+    if (this.props.sites.length) {
+      const markersLatLong = this.props.sites.map(site => ({
+        latitude: site.latitude,
+        longitude: site.longitude
+      }));
+      this.mapRef.fitToCoordinates(markersLatLong, true);
+    }
+  };
+
   render() {
-    console.log(this.props);
-    return <MapView style={styles.mapView}>{this.generateMarkers()}</MapView>;
+    return (
+      <MapView
+        ref={ref => {
+          this.mapRef = ref;
+        }}
+        style={styles.mapView}
+      >
+        {this.generateMarkers()}
+      </MapView>
+    );
   }
 }
 
@@ -41,7 +72,8 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchSites: () => dispatch(fetchSites())
+  fetchSites: () => dispatch(fetchSites()),
+  siteHasTreasure: id => dispatch(fetchTreasure(id))
 });
 
 export default connect(
--- Redux/sites.js
+++ Redux/sites.js
@@ -1,4 +1,5 @@
 import { treasureApi } from "../API/treasureAPI";
+import { Vibration, Alert } from "react-native";
 
 // Sites Reducer
 
@@ -12,12 +13,20 @@ export const reducer = (state = initialState, action) => {
       return { sites: action.payload, isFetching: false, errorFetching: null };
     case FETCHING_SITES_FAILED:
       return { sites: [], isFetching: false, errorFetching: action.payload };
+    case SITE_HAS_TREASURE:
+      if (action.payload) {
+        Alert.alert("😄", "You found treasure!");
+        Vibration.vibrate(3000);
+      } else {
+        Alert.alert("😔", "No treasure here.");
+      }
+      return state;
     default:
       return state;
   }
 };
 
-// Async action creator
+// Async action creators
 
 export const fetchSites = () => dispatch => {
   dispatch(fetchingSites());
@@ -25,6 +34,12 @@ export const fetchSites = () => dispatch => {
     .getTreasureSites()
     .then(sites => dispatch(addSites(sites)))
     .catch(error => dispatch(fetchingSitesFailed(error)));
+};
+
+export const fetchTreasure = id => dispatch => {
+  treasureApi
+    .siteHasTreasure(id)
+    .then(hasTreasure => dispatch(siteHasTreasure(hasTreasure)));
 };
 
 // Action creators
@@ -43,8 +58,14 @@ const fetchingSitesFailed = errorMessage => ({
   payload: errorMessage
 });
 
+const siteHasTreasure = hasTreasure => ({
+  type: SITE_HAS_TREASURE,
+  payload: hasTreasure
+});
+
 // Action types
 
 const FETCHING_SITES = "FETCHING_SITES";
 const ADD_SITES = "ADD_SITES";
 const FETCHING_SITES_FAILED = "FETCHING_SITES_FAILED";
+const SITE_HAS_TREASURE = "SITE_HAS_TREASURE";
